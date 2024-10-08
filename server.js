e6f09cc0485b5d9db6e04b2ed84a0e7d1b0cfa56
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-08 17:22:14 +0200

    Create query string of id, and if statement

--- server.js
+++ server.js
@@ -96,6 +96,17 @@ app.get('/api/ideas', (req, res) => {
   res.json({ success: true, data: ideas })
 })
 
+app.get('/api/ideas/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id)
+  if (!idea) {
+    return res.status(404).json({ success: false, error: 'Resource not found' })
+  }
+  res.json({
+    success: true,
+    data: idea,
+  })
+})
+
 app.get('/api/technologies', (req, res) => {
   res.json({ success: true, data: technologies })
 })
