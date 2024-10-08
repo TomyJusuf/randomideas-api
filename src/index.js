fb11dd39f5f40480fff896a784856fd2ddc447b4
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-08 21:19:20 +0200

    Fetch data into HTML

--- src/index.js
+++ src/index.js
@@ -21,7 +21,12 @@ class FetchData {
 }
 
 const fetchData1 = new FetchData(url, '/')
-fetchData1.get().then((data) => console.log(data))
+
+fetchData1.get().then((data) => {
+  const div = document.createElement('div')
+  div.innerText = `${data}`
+  document.getElementById('app').appendChild(div)
+})
 
 const fetchData2 = new FetchData(url, '/api/ideas')
 fetchData2.get().then((data) => console.log(data))
