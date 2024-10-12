72ae2e784d48d117eda6548cf07fec77d046d05d
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-12 21:23:45 +0200

    Import Modal and IdeaInputForm components, declare and initialize them

--- client/src/index.js
+++ client/src/index.js
@@ -2,3 +2,5 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import './css/style.css'
 import Modal from './components/Modal'
 const modal = new Modal()
+import IdeaInputForm from './components/IdeaInputForm'
+const input = new IdeaInputForm()
